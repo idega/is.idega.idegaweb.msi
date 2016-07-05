74c18759b65905a09988d1e9c6ef6079fc59faa6
lapiukshtiss <martynas.stake@gmail.com>
2016-07-05 11:10:07 +0300

    Styles for Race number editor

--- src/java/is/idega/idegaweb/msi/presentation/RaceNumberEditor.java
+++ src/java/is/idega/idegaweb/msi/presentation/RaceNumberEditor.java
@@ -1,24 +1,30 @@
 package is.idega.idegaweb.msi.presentation;
 
 import is.idega.idegaweb.msi.data.RaceNumber;
+import is.idega.idegaweb.msi.data.RaceNumberHome;
 import is.idega.idegaweb.msi.data.RaceType;
+import is.idega.idegaweb.msi.data.RaceTypeHome;
 import is.idega.idegaweb.msi.data.RaceUserSettings;
 import is.idega.idegaweb.msi.util.MSIConstants;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
+import java.util.logging.Level;
 
 import javax.ejb.CreateException;
 import javax.ejb.FinderException;
+import javax.faces.context.FacesContext;
 
+import com.idega.data.IDOLookup;
+import com.idega.data.IDOLookupException;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Layer;
 import com.idega.presentation.Table2;
 import com.idega.presentation.TableCell2;
 import com.idega.presentation.TableRow;
 import com.idega.presentation.TableRowGroup;
-import com.idega.presentation.text.Break;
 import com.idega.presentation.text.Link;
 import com.idega.presentation.text.Text;
 import com.idega.presentation.ui.DropdownMenu;
@@ -28,6 +34,9 @@ import com.idega.presentation.ui.SubmitButton;
 import com.idega.presentation.ui.TextInput;
 import com.idega.user.data.User;
 import com.idega.util.IWTimestamp;
+import com.idega.util.ListUtil;
+import com.idega.util.PresentationUtil;
+import com.idega.util.StringUtil;
 import com.idega.util.text.Name;
 
 public class RaceNumberEditor extends RaceBlock {
@@ -42,6 +51,49 @@ public class RaceNumberEditor extends RaceBlock {
 	private static final int ACTION_VIEW = 1;
 	private static final int ACTION_EDIT = 2;
 	private static final int ACTION_SAVE = 3;
+
+	private RaceNumberHome getRaceNumberHome() {
+		try {
+			return (RaceNumberHome) IDOLookup.getHome(RaceNumber.class);
+		} catch (IDOLookupException e) {
+			getLogger().log(Level.WARNING, 
+					"Failed to get " + RaceNumberHome.class + " cause of: ", e);
+		}
+
+		return null;
+	}
+
+	private RaceTypeHome getRaceTypeHome() {
+		try {
+			return (RaceTypeHome) IDOLookup.getHome(RaceType.class);
+		} catch (IDOLookupException e) {
+			getLogger().log(Level.WARNING, 
+					"Failed to get " + RaceTypeHome.class + " cause of: ", e);
+		}
+
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.idega.presentation.Block#encodeBegin(javax.faces.context.FacesContext)
+	 */
+	@Override
+	public void encodeBegin(FacesContext fc) throws IOException {
+		super.encodeBegin(fc);
+
+		if (getSportUnionBundle() != null) {
+			ArrayList<String> styleSheets = new ArrayList<String>();
+			styleSheets.add(getSportUnionBundle().getVirtualPathWithFileNameString("style/viewer.css"));
+			styleSheets.add(getSportUnionBundle().getVirtualPathWithFileNameString("style/select.css"));
+			styleSheets.add(getSportUnionBundle().getVirtualPathWithFileNameString("style/input.css"));
+			styleSheets.add(getSportUnionBundle().getVirtualPathWithFileNameString("style/button.css"));
+			styleSheets.add(getSportUnionBundle().getVirtualPathWithFileNameString("style/label.css"));
+			styleSheets.add(getSportUnionBundle().getVirtualPathWithFileNameString("style/sport_union.css"));
+			PresentationUtil.addStyleSheetsToHeader(
+					IWContext.getIWContext(fc), styleSheets);
+		}
+	}
 
 	public void main(IWContext iwc) throws Exception {
 		init(iwc);
@@ -68,40 +120,91 @@ public class RaceNumberEditor extends RaceBlock {
 		}
 		return ACTION_VIEW;
 	}
-	
-	protected void showList(IWContext iwc) throws RemoteException, FinderException {
-		
 
-		this.getParentPage().addStyleSheetURL(getBundle().getVirtualPathWithFileNameString("msiEditor.css"));
+	private DropdownMenu getRaceTypeDropdown(IWContext iwc) {
+		DropdownMenu raceTypeDropDown = (DropdownMenu) getStyledInterface(new DropdownMenu(PARAMETER_RACE_TYPE_PK)); 
+		raceTypeDropDown.addMenuElement("", localize(
+				"race_number_editor.select_race_type", 
+				"Select race type"));
+		raceTypeDropDown.setToSubmit();
+
+		Collection<RaceType> raceTypes = getRaceBusiness(iwc).getRaceTypes();
+		for (RaceType raceType: raceTypes) {
+			raceTypeDropDown.addMenuElement(
+					raceType.getPrimaryKey().toString(), 
+					localize(raceType.getRaceType(), raceType.getRaceType()));
+		}
+
+		if (iwc.isParameterSet(PARAMETER_RACE_TYPE_PK)) {
+			raceTypeDropDown.setSelectedElement(iwc.getParameter(PARAMETER_RACE_TYPE_PK));
+		}
+
+		return raceTypeDropDown;
+	}
+
+	protected void showList(IWContext iwc) throws RemoteException, FinderException {
+		String styleSheets = getBundle().getVirtualPathWithFileNameString("msiEditor.css");
+		if (!StringUtil.isEmpty(styleSheets)) {
+			this.getParentPage().addStyleSheetURL(styleSheets);
+		}
+
 		Form form = new Form();
-				
+
 		Table2 table = new Table2();
 		table.setWidth("100%");
 		table.setCellpadding(0);
 		table.setCellspacing(0);
-		table.setStyleClass("msiracenumbereditor");
-				
-		Collection raceTypes = getRaceBusiness(iwc).getRaceTypes();
-		Iterator raceTypeIt = raceTypes.iterator();
-		DropdownMenu raceTypeDropDown = (DropdownMenu) getStyledInterface(new DropdownMenu(PARAMETER_RACE_TYPE_PK)); 
-		raceTypeDropDown.addMenuElement("", localize("race_number_editor.select_race_type","Select race type"));
-		while (raceTypeIt.hasNext()) {
-			RaceType raceType = (RaceType)raceTypeIt.next();
-			raceTypeDropDown.addMenuElement(raceType.getPrimaryKey().toString(), localize(raceType.getRaceType(),raceType.getRaceType()));
-		}
-		raceTypeDropDown.setToSubmit();
-		
-		Collection numbers = null;
+		table.setStyleClass("msiracenumbereditor table");
+
+		TableRowGroup group = table.createHeaderRowGroup();
+		TableRow row = null;
+		TableCell2 cell = null;
+
 		if (iwc.isParameterSet(PARAMETER_RACE_TYPE_PK)) {
-			String raceTypeID = iwc.getParameter(PARAMETER_RACE_TYPE_PK);
-			raceTypeDropDown.setSelectedElement(raceTypeID);
-			RaceType selectedRaceType = getRaceBusiness(iwc).getRaceTypeHome().findByPrimaryKey(Integer.valueOf(raceTypeID));
-			numbers = getRaceBusiness(iwc).getRaceNumberHome().findAllByType(selectedRaceType); 
-			if (numbers == null || numbers.isEmpty()) {
+			row = group.createRow();
+			cell = row.createHeaderCell();
+			cell.setStyleClass("text");
+			cell.add(new Text(localize("race_number_editor.race_number", "Number")));
+
+			cell = row.createHeaderCell();
+			cell.setStyleClass("text");
+			cell.add(new Text(localize("race_number_editor.race_type", "Race type")));
+
+			cell = row.createHeaderCell();
+			cell.setStyleClass("text");
+			cell.add(new Text(localize("race_number_editor.application_date", "Application date")));
+
+			cell = row.createHeaderCell();
+			cell.setStyleClass("text");
+			cell.add(new Text(localize("race_number_editor.approved_date", "Approved date")));
+
+			cell = row.createHeaderCell();
+			cell.setStyleClass("text");
+			cell.add(new Text(localize("race_number_editor.user_ssn", "User ssn")));
+
+			cell = row.createHeaderCell();
+			cell.setStyleClass("text");
+			cell.add(new Text(localize("race_number_editor.user_name", "User name")));
+
+			cell = row.createHeaderCell();
+			cell.setStyleClass("text");
+			cell.add(new Text(localize("edit", "Edit")));
+		}
+
+		group = table.createBodyRowGroup();
+		int iRow = 1;
+
+		Collection<RaceNumber> numbers = null;
+
+		String raceTypeID = iwc.getParameter(PARAMETER_RACE_TYPE_PK);
+		if (!StringUtil.isEmpty(raceTypeID)) {
+			RaceType selectedRaceType = getRaceTypeHome().findByPrimaryKey(Integer.valueOf(raceTypeID));
+			numbers = getRaceNumberHome().findAllByType(selectedRaceType); 
+			if (ListUtil.isEmpty(numbers)) {
 				for (int i = 0; i < 1000; i++) {
 					RaceNumber number;
 					try {
-						number = getRaceBusiness(iwc).getRaceNumberHome().create();
+						number = getRaceNumberHome().create();
 						number.setRaceNumber(Integer.toString(i));
 						number.setRaceType(selectedRaceType);
 						number.setIsApproved(false);
@@ -111,122 +214,110 @@ public class RaceNumberEditor extends RaceBlock {
 						e.printStackTrace();
 					}
 				}
-				
-				numbers = getRaceBusiness(iwc).getRaceNumberHome().findAllByType(selectedRaceType);
+
+				numbers = getRaceNumberHome().findAllByType(selectedRaceType);
 			}
 		}
-		
-		TableRowGroup group = table.createHeaderRowGroup();
-		TableRow row = group.createRow();
-		TableCell2 cell = row.createHeaderCell();
-		cell.setCellHorizontalAlignment(Table2.HORIZONTAL_ALIGNMENT_LEFT);
-		cell.add(raceTypeDropDown);
-		group.createRow().createCell().setHeight("20");
-		
-		if (iwc.isParameterSet(PARAMETER_RACE_TYPE_PK)) {
-			row = group.createRow();
-			cell = row.createHeaderCell();
-			cell.setCellHorizontalAlignment(Table2.HORIZONTAL_ALIGNMENT_LEFT);
-			cell.add(new Text(localize("race_number_editor.race_number", "Number")));
 
-			cell = row.createHeaderCell();
-			cell.setCellHorizontalAlignment(Table2.HORIZONTAL_ALIGNMENT_LEFT);
-			cell.add(new Text(localize("race_number_editor.race_type", "Race type")));
-
-			cell = row.createHeaderCell();
-			cell.setCellHorizontalAlignment(Table2.HORIZONTAL_ALIGNMENT_LEFT);
-			cell.add(new Text(localize("race_number_editor.application_date", "Application date")));
-
-			cell = row.createHeaderCell();
-			cell.setCellHorizontalAlignment(Table2.HORIZONTAL_ALIGNMENT_LEFT);
-			cell.add(new Text(localize("race_number_editor.approved_date", "Approved date")));
-
-			cell = row.createHeaderCell();
-			cell.setCellHorizontalAlignment(Table2.HORIZONTAL_ALIGNMENT_LEFT);
-			cell.add(new Text(localize("race_number_editor.user_ssn", "User ssn")));
-
-			cell = row.createHeaderCell();
-			cell.setCellHorizontalAlignment(Table2.HORIZONTAL_ALIGNMENT_LEFT);
-			cell.add(new Text(localize("race_number_editor.user_name", "User name")));
-		}
-				
-		group = table.createBodyRowGroup();
-		int iRow = 1;
-		
 		if (numbers != null) {
-			Iterator iter = numbers.iterator();
-			RaceNumber number;
-			
-			while (iter.hasNext()) {
+			for (RaceNumber number : numbers) {
 				row = group.createRow();
-				number = (RaceNumber) iter.next();
 				try {
 					Link edit = new Link(getEditIcon(number.getRaceNumber()));
 					edit.addParameter(PARAMETER_RACE_TYPE_PK, iwc.getParameter(PARAMETER_RACE_TYPE_PK));
 					edit.addParameter(PARAMETER_RACE_NUMBER_PK, number.getPrimaryKey().toString());
 					edit.addParameter(PARAMETER_ACTION, ACTION_EDIT);
-								
+
 					cell = row.createCell();
+					cell.setStyleClass("text");
 					cell.add(new Text(number.getRaceNumber()));
+
 					cell = row.createCell();
+					cell.setStyleClass("text");
 					if (number.getRaceType() != null) {
-						cell.add(new Text(localize(number.getRaceType().getRaceType(), number.getRaceType().getRaceType())));
+						cell.add(new Text(localize(
+								number.getRaceType().getRaceType(), 
+								number.getRaceType().getRaceType())));
 					} else {
 						cell.add(new Text(""));						
 					}
+
 					cell = row.createCell();
+					cell.setStyleClass("text");
 					if (number.getApplicationDate() != null) {
 						cell.add(new Text(new IWTimestamp(number.getApplicationDate()).getDateString("dd.MM.yyyy")));
 					} else {
 						cell.add(new Text(""));						
 					}
+
 					cell = row.createCell();
+					cell.setStyleClass("text");
 					if (number.getApprovedDate() != null) {
 						cell.add(new Text(new IWTimestamp(number.getApprovedDate()).getDateString("dd.MM.yyyy")));						
 					} else {
 						cell.add(new Text(""));												
 					}
+
 					cell = row.createCell();
+					cell.setStyleClass("text");
 					RaceUserSettings settings = null;
 					if (number.getRaceType().getRaceType().equals(MSIConstants.RACE_TYPE_SNOCROSS)) {
 						try {
 							settings = getRaceBusiness(iwc).getRaceUserSettingsHome().findBySnocrossRaceNumber(number);
-						} catch (Exception e) {
-						}
+						} catch (Exception e) {}
 					} else if (number.getRaceType().getRaceType().equals(MSIConstants.RACE_TYPE_MX_AND_ENDURO)) {
 						try {
 							settings = getRaceBusiness(iwc).getRaceUserSettingsHome().findByMXRaceNumber(number);
-						} catch (Exception e) {
-						}						
+						} catch (Exception e) {}
 					}
+
 					if (settings != null) {
 						User user = settings.getUser();
 						cell.add(new Text(user.getPersonalID()));
 						cell = row.createCell();
-						cell.add(new Text(new Name(user.getFirstName(), user.getMiddleName(), user.getLastName()).getName()));
+						cell.add(new Text(new Name(
+								user.getFirstName(), 
+								user.getMiddleName(), 
+								user.getLastName()).getName()));
 					} else {
-						cell.add(new Text(""));												
+						cell.add(new Text(""));
 						cell = row.createCell();
-						cell.add(new Text(""));												
+						cell.add(new Text(""));
 					}
+
 					row.createCell().add(edit);
 				}
 				catch (Exception ex) {
 					ex.printStackTrace();
 				}
-				
+
 				if (iRow % 2 == 0) {
-					row.setStyleClass("evenRow");
+					row.setStyleClass("tableRow even evenRow");
 				} else {
-					row.setStyleClass("oddRow");
+					row.setStyleClass("tableRow odd oddRow");
 				}
+
 				iRow++;
 			}
 		}
+
+		Label raceTypeDropdownLabel = new Label();
+		raceTypeDropdownLabel.setLabel(getLocalizedString("race_editor.race_type", "Type", iwc));
+
+		Layer raceTypeLayer = new Layer();
+		raceTypeLayer.setStyleClass("formItem");
+		raceTypeLayer.add(raceTypeDropdownLabel);
+		raceTypeLayer.add(getRaceTypeDropdown(iwc));
+
+		Layer filterLayer = new Layer();
+		filterLayer.setStyleClass("formSection navigationLayer");
+		filterLayer.add(raceTypeLayer);
+
+		form.add(filterLayer);
 		form.add(table);
 		add(form);
 	}
-	
+
 	public void showEditor(IWContext iwc) throws java.rmi.RemoteException, FinderException {
 		String raceNumberID = iwc.getParameter(PARAMETER_RACE_NUMBER_PK);
 		Form form = new Form();
@@ -247,51 +338,68 @@ public class RaceNumberEditor extends RaceBlock {
 		if (eNumber.getRaceType().getRaceType().equals(MSIConstants.RACE_TYPE_SNOCROSS)) {
 			try {
 				settings = getRaceBusiness(iwc).getRaceUserSettingsHome().findBySnocrossRaceNumber(eNumber);
-			} catch (Exception e) {
-			}
+			} catch (Exception e) {}
 		} else if (eNumber.getRaceType().getRaceType().equals(MSIConstants.RACE_TYPE_MX_AND_ENDURO)) {
 			try {
 				settings = getRaceBusiness(iwc).getRaceUserSettingsHome().findByMXRaceNumber(eNumber);
-			} catch (Exception e) {
-			}						
+			} catch (Exception e) {}						
 		}
 		
 		TextInput user = new TextInput(PARAMETER_USER_SSN);
 		if (settings != null) {
 			user.setValue(settings.getUser().getPersonalID());
 		}
+
+		Layer formSectionLayer = new Layer();
+		formSectionLayer.setStyleClass("formSection");
 		
 		Layer layer = new Layer(Layer.DIV);
 		layer.setStyleClass(STYLENAME_FORM_ELEMENT);
+		layer.setStyleClass("formItem");
 		Label label = new Label(localize("race_number_editor.number", "Number"), number);
 		layer.add(label);
 		layer.add(number);
-		form.add(layer);
-		form.add(new Break());
+		formSectionLayer.add(layer);
 
 		layer = new Layer(Layer.DIV);
 		layer.setStyleClass(STYLENAME_FORM_ELEMENT);
+		layer.setStyleClass("formItem");
 		label = new Label(localize("race_number_editor.race_type", "Type"), type);
 		layer.add(label);
 		layer.add(type);
-		form.add(layer);
-		form.add(new Break());
+		formSectionLayer.add(layer);
 
 		layer = new Layer(Layer.DIV);
 		layer.setStyleClass(STYLENAME_FORM_ELEMENT);
+		layer.setStyleClass("formItem");
 		label = new Label(localize("race_number_editor.user_ssn", "User ssn"), user);
 		layer.add(label);
 		layer.add(user);
-		form.add(layer);
-		form.add(new Break());
-				
-		SubmitButton save = (SubmitButton) getButton(new SubmitButton(localize("save", "Save"), PARAMETER_ACTION, String.valueOf(ACTION_SAVE)));
+		formSectionLayer.add(layer);
 
+		SubmitButton save = new SubmitButton(
+				localize("save", "Save"), 
+				PARAMETER_ACTION, 
+				String.valueOf(ACTION_SAVE));
+		save.setStyleClass("button");
 
-		SubmitButton cancel = (SubmitButton) getButton(new SubmitButton(localize("cancel", "Cancel"), PARAMETER_ACTION, String.valueOf(ACTION_VIEW)));
+		SubmitButton cancel = new SubmitButton(
+				localize("cancel", "Cancel"), 
+				PARAMETER_ACTION, 
+				String.valueOf(ACTION_VIEW));
+		cancel.setStyleClass("button");
 
-		form.add(save);
-		form.add(cancel);
+		Layer buttonLayer = new Layer();
+		buttonLayer.setStyleClass("buttonLayer");
+		buttonLayer.add(save);
+		buttonLayer.add(cancel);
+		
+		Layer formContent = new Layer();
+		formContent.setStyleClass("formContents");
+		formContent.add(formSectionLayer);
+		formContent.add(buttonLayer);
+		
+		form.add(formContent);
 
 		add(form);
 	}
